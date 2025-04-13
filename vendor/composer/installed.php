528ef0167ef97155311673082a5db6b75c5896af
mmrho <mmohammadrezaho@gmail.com>
2025-04-13 20:04:56 +0330

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => null,
+        'name' => 'sibaneh/wp-theme-sibaneh',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sibaneh/wp-theme-sibaneh' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => null,
+            'dev_requirement' => false,
+        ),
+    ),
+);
